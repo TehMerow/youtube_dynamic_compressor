7d288466ef6e5e24bc05926719d3d84820b9fd37
TehMerow <64508177+TehMerow@users.noreply.github.com>
2020-07-16 16:55:30 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# YouTube Dynamic Compressor Addon
---
+++ yt_compressor.js
@@ -0,0 +1,27 @@
+(function(){
+    // TODO
+    // add a toggle button on the page where you can turn it on and off
+    // turn this into an addon
+      
+    function create_audio_compressor(){
+      var video = document.querySelector('video');
+        var audioCtx = new AudioContext()
+      var gainVal = 100
+      var source = audioCtx.createMediaElementSource(video);
+      var compressor = audioCtx.createDynamicsCompressor()
+    
+      compressor.threshold.value = -gainVal
+      compressor.ratio.value = 20
+      compressor.attack.value = 0.1
+      compressor.release.value = 1.0
+    
+      source.connect(compressor)
+      compressor.connect(audioCtx.destination)
+      let message_style = "color: white; background-color: hsl(200, 90%, 50%)"
+      console.log('%c dynamic compressor active, turn off script and reload to deactivate', message_style)
+      
+    }
+    
+    
+    create_audio_compressor()
+    })()
