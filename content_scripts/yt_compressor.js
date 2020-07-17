abfbc2231bb4f35a3f595b4b1b9bc188ef062a7c
TehMerow <64508177+TehMerow@users.noreply.github.com>
2020-07-16 17:41:01 -0700

    added compressor and popup code

--- content_scripts/yt_compressor.js
+++ content_scripts/yt_compressor.js
@@ -4,10 +4,10 @@
     }
 
     window.hasRun = true;
-
+   
+    var audioCtx = new AudioContext()
     function create_audio_compressor(){
       var video = document.querySelector('video');
-        var audioCtx = new AudioContext()
       var gainVal = 100
       var source = audioCtx.createMediaElementSource(video);
       var compressor = audioCtx.createDynamicsCompressor()
@@ -17,13 +17,32 @@
       compressor.attack.value = 0.1
       compressor.release.value = 1.0
     
-      source.connect(compressor)
-      compressor.connect(audioCtx.destination)
+ 
       let message_style = "color: white; background-color: hsl(200, 90%, 50%)"
       console.log('%c dynamic compressor active, turn off script and reload to deactivate', message_style)
       
     }
-    
-    
+
+    function connect_compressor(){
+      source.disconnect(audioCtx.destination)
+      source.connect(compressor)
+      compressor.connect(audioCtx.destination)
+    }
+    function disconect_compressor(){
+      source.disconnect(compressor)
+      compressor.disconnect(audioCtx.destination)
+      source.connect(audioCtx.destination);
+    }
     create_audio_compressor()
+    
+
+    browser.runtime.onMessage.addListener((message) => {
+      if (message.command === "compress_active"){
+        connect_compressor()
+      }
+      else if (message.command == "compress_inactive"){
+        disconect_compressor()
+      }
+    })
+    
     })()
